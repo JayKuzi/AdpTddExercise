2206a827cbbee635e0aac0f4b57f84d938f4bbfd
Jody Kearns <kearnsjody@gmail.com>
2022-02-04 17:23:32 +0200

    Adding my section of the exercise

--- src/main/java/za/ac/cput/JodyKearns.java
+++ src/main/java/za/ac/cput/JodyKearns.java
@@ -1,4 +1,46 @@
 package za.ac.cput;
 
 public class JodyKearns {
+    private double measurement;
+    private String unitOfMeasurement;
+
+    public JodyKearns(double measurement, String unitOfMeasurement) {
+        this.measurement = measurement;
+        this.unitOfMeasurement = unitOfMeasurement;
+    }
+
+    public String measurementConverter() {
+
+        switch (unitOfMeasurement.toUpperCase()) {
+            case "CM":
+                return "The measurement in Inches is: " + (measurement / 2.54);
+            case "F":
+                return "The measurement in Centimeters is: " + (measurement * 2.54);
+            default:
+                return "Invalid unit of measurement";
+        }
+
+    }
+
+    public double getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(double measurement) {
+        this.measurement = measurement;
+    }
+
+    public String getUnitOfMeasurement() {
+        return unitOfMeasurement;
+    }
+
+    public void setUnitOfMeasurement(String unitOfMeasurement) {
+        this.unitOfMeasurement = unitOfMeasurement;
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement = " + measurement + ", Unit of measurement = " + unitOfMeasurement;
+    }
+
 }
--- src/test/java/za/ac/cput/JodyKearnsTest.java
+++ src/test/java/za/ac/cput/JodyKearnsTest.java
@@ -1,7 +1,66 @@
 package za.ac.cput;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JodyKearnsTest {
+    private JodyKearns converter1;
+    private JodyKearns converter2;
+    private JodyKearns converter3;
+    private JodyKearns converter4;
 
+    @BeforeEach
+    void setUp(){
+        converter1 = new JodyKearns(10,"CM");
+        converter2 = new JodyKearns(10,"CM");
+        converter3 = converter1;
+        converter4 = new JodyKearns(20,"KM");
+    }
+
+    //Testing for object equality.
+    @Test
+    void testEquality(){
+        assertEquals(converter1,converter3);
+        assertNotEquals(converter1,converter2);
+
+    }
+
+    //Testing for object identity.
+    @Test
+    void testIdentity(){
+        assertSame(converter1,converter3);
+        assertNotSame(converter1,converter2);
+    }
+
+    //Failing Test
+    @Test
+    void failingTest(){
+
+        String str3 = converter1.measurementConverter();
+        String str4 = converter2.measurementConverter();
+        assertEquals(str3,str4);
+        fail("Purposefully failing this test as method for Kilometer conversion has not been created yet");
+    }
+
+
+    //Timeout Test
+    @Test
+    @Timeout(value = 1000,unit = TimeUnit.MILLISECONDS)   //I set the value to a high number to ensure that the test does not timeout and fail.
+    void timeoutTest(){
+        String str1 = converter1.measurementConverter();
+        String str2 = converter2.measurementConverter();
+        assertEquals(str1,str2);
+
+    }
+
+    //Disabling Test
+    @Disabled("Disabled as I know it will fail")
+    @Test
+    void disabledTest(){
+        assertEquals(converter1,converter2);
+    }
 }
