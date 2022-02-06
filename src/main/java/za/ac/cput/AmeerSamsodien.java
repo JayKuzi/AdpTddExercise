a697a226fe200de4d451396c36585970444707f2
Ameer <220005060@mycput.ac.za>
2022-02-06 22:02:22 +0200

    Ameer trying to commit

--- src/main/java/za/ac/cput/AmeerSamsodien.java
+++ src/main/java/za/ac/cput/AmeerSamsodien.java
@@ -1,4 +1,20 @@
 package za.ac.cput;
 
+import java.util.Scanner;
+
 public class AmeerSamsodien {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
 }
+
--- src/test/java/za/ac/cput/AmeerSamsodienTest.java
+++ src/test/java/za/ac/cput/AmeerSamsodienTest.java
@@ -1,4 +1,40 @@
 package za.ac.cput;
 
-public class AmeerSamsodienTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmeerSamsodienTest {
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    void testEquality(){
+
+    }
+
+    @Test
+    void testIdentity(){
+
+    }
+
+    @Test
+    void failingTest(){
+
+    }
+
+    @Test
+    //@Timeout()
+    void timeoutTest(){
+
+    }
+
+    @Disabled
+    @Test
+    void disabledTest(){
+
+    }
 }
